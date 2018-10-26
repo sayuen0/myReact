865efcb7e62a1654b08662f45e14fd73a581f9bd
Akira Kosuda <sayuen0@gmail.com>
2018-10-26 10:03:48 +0900

    init

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var element = React.createElement('h1', {
+    className: "greeting"
+}, "Hello, World!");
+ReactDOM.render(element, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
